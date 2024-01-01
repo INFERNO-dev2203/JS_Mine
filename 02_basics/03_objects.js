d26e2da3d5d4ac036149a42b6370edc1dca1d25f
Prajjwal Pandey <pandeyprajjwal226@gmail.com>
2024-01-01 15:44:15 +0530

    Object Basics Completed

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,15 +1,17 @@
+// 3 Ways of declaring an object in JS
 // singleton
-// Object.create
+// Object Constructors - Using Object.create
+// Object literals
 
-// object literals
+const mySym = Symbol("key1")  // This way we can create a new symbol
 
-const mySym = Symbol("key1")
+// ---------------------------------------------------------------
 
-
+// Object literals
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    [mySym]: "mykey1",              // We can use the symbol declared as property of a object using this syntax. Remember to add [] while using the name of symbol as a key. This would not throw an error but in that case if we print the typeof mySym it will show us string.
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -21,18 +23,40 @@ const JsUser = {
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
+// console.log(typeof(JsUser[mySym]))       Prints String. But correct usage of symbol is this only.
+
+
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
+// Object.freeze(JsUser)   // freeze is used to freeze the values of properties of object after this if any changes are made to the value of object then its value would not be changed
+JsUser.email = "hitesh@microsoft.com"  // Since this change is made to the JsUser object after using freeze this change will not propogate 
+// console.log(JsUser);  // Prints the older value of object (before calling freeze).
 
+
+// We can append functions to objects as key value pair. Here in this case greeting will be the key and this function's refrence will be the value of that key.
 JsUser.greeting = function(){
-    console.log("Hello JS user");
+    // console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    // console.log(`Hello JS user, ${this.name}`);
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// console.log(JsUser.greeting());      // Prints Hello Js User
+
+// console.log(JsUser.greetingTwo());   // Prints Hello Js User, Hitesh
+
+// console.log(JsUser);         // Prints 
+//  {
+//   name: 'Hitesh',
+//   'full name': 'Hitesh Choudhary',
+//   age: 18,
+//   location: 'Jaipur',
+//   email: 'hitesh@microsoft.com',
+//   isLoggedIn: false,
+//   lastLoginDays: [ 'Monday', 'Saturday' ],
+//   greeting: [Function (anonymous)],
+//   greetingTwo: [Function (anonymous)],
+//   [Symbol(key1)]: 'mykey1'
+// }    
+
+
--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,5 +1,5 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object()  - This creates a singleton object
+ const tinderUser = {} // This is a non singleton object.
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -7,6 +7,8 @@ tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
 
+
+// We can use object nesting in following way.
 const regularUser = {
     email: "some@gmail.com",
     fullname: {
@@ -18,18 +20,27 @@ const regularUser = {
 }
 
 // console.log(regularUser.fullname.userfullname.firstname);
+// We can use multiple dot operators to go deep inside the nesting like we are accessing the firstname of the user in this case.
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+//const obj3 = { obj1, obj2 }     // Prints { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+// This is clearly a wrong way of combining 2 different objects if we do so we get object of objects. Instead of a single object.
 
-const obj3 = {...obj1, ...obj2}
+
+// Instead we can use Object.assign method to assign multiple objects into single object. 
+// In this syntax the first empty object serves as the target and rest all other objects serves as a source. 
+// The catch is if we remove the empty object from the 1st arguement passed inside the assign function then it will assign all the objects into obj1.
+// const obj3 = Object.assign({}, obj1, obj2, obj4) 
+
+
+// We can use spread operator like used below to add multiple objects to single objects.
+// const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
-
+// Scenario - WE GOT USERS ARRAY OF OBJECTS AND WE WANT TO ACCESS IT.
 const users = [
     {
         id: 1,
@@ -45,28 +56,36 @@ const users = [
     },
 ]
 
-users[1].email
+// This could be one of the ways to access the objects but it is not very useful because not necessarily everytime we know the number/index of required object in array.
+// console.log(users[1].email)
+
+
+// 2nd way of accessing it is using Object.keys or Object.values or Object.entries
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser));        // This Prints all the keys of tiderUser object in the form of an array.
+// console.log(Object.values(tinderUser));      // This Prints all the values(of Keys/Properties) of tinderUser object in the form of an array.
 // console.log(Object.entries(tinderUser));
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));        // We can check any object that its having a specific property or not using .hasOwnProperty.
+// If the asked property exists then in that case it will return true else false.
+
+// OBJECT DESTRUCTURING - Process of taking a single key/Property to store it in a variable is known as Destructuring of object.
+// const course = {
+//     coursename: "js in hindi",
+//     price: "999",
+//     courseInstructor: "hitesh"
+// }
+
+// course.courseInstructor   // Accessing this value using this syntax is quite complex so here we use destructuring.
 
 
-const course = {
-    coursename: "js in hindi",
-    price: "999",
-    courseInstructor: "hitesh"
-}
+// HERE WE ARE DESTRUCTURING THE key CourseInstructor saperately, by this we can access courseInstructor value directly by typing courseInstructor instead of course.courseInstructor.
 
-// course.courseInstructor
-
-const {courseInstructor: instructor} = course
+// const {courseInstructor} = course
 
 // console.log(courseInstructor);
-console.log(instructor);
+// console.log(instructor);
 
 // {
 //     "name": "hitesh",
@@ -74,9 +93,9 @@ console.log(instructor);
 //     "price": "free"
 // }
 
-[
-    {},
-    {},
-    {}
-]
+// [
+//     {},
+//     {},
+//     {}
+// ]
 
